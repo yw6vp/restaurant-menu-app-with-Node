df052b5d261f7ebbdf848be086fcdd7bcc06bb35
Yunxiao Wang <yw6vp@virginia.edu>
2016-03-31 00:27:19 -0400

    Add OAUTH with facebook for authentication.

--- config.js
+++ config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '1718945971684359',
+        clientSecret: 'da3be0f0782672848e8d07f132450416',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
 }
